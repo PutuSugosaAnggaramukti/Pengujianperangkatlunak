9e99db0a22d9ed9bdfe40cac46b96feedeb3ea5f
putuanggaramukti <putu.sugosa@si.ukdw.ac.id>
2021-04-28 23:18:01 +0700

    test case uat

---
+++ My First Mobile Project/Test Suites/Uji Coba Login dengan Data Test.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Uji Coba Login dengan Data Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>076af7b3-af65-4a95-ab98-e10a21a3e4e3</testSuiteGuid>
+   <testCaseLink>
+      <guid>49323630-88a2-4087-9fa3-e383b6bd6d1a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/pengujian login dengan variabel</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>8bf2ff05-509d-4d16-895f-a7daf6a3879c</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Data Login</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>8bf2ff05-509d-4d16-895f-a7daf6a3879c</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Username</value>
+         <variableId>7cfe61c0-66cc-4e83-b48b-3ba7ccd19f15</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>8bf2ff05-509d-4d16-895f-a7daf6a3879c</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
+         <variableId>de206e98-1d6d-4127-a1d7-a90eaa5ec1b0</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
